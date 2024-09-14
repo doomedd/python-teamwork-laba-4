d39f37650258d1c5d2cc43b8d1d15aa07bcd3ce9
d4nchq <danichrudenko@gmail.com>
2024-09-14 19:09:50 +0300

    Update КН 33 лаба 4.py

--- КН 33 лаба 4.py
+++ КН 33 лаба 4.py
@@ -12,3 +12,22 @@ print(f"Text in lowercase: {lowercase_text}")
 # 3. Функція count() для підрахунку кількості слова 'is' у тексті
 count_is = text.count('is')
 print(f"The word 'is' appears {count_is} times in the text")
+
+# Руденко Данііл (КН-33/2), рядки 16-33.
+# 4. # Функція count() для підрахунку кількості слова 'his' у тексті.
+
+count_his = text.count('his')
+print(f"The word 'his' appears {count_his} times in the text")
+
+# 5. Функція split() для розділення тексту на окремі слова.
+
+words = text.split()
+print(f"List of words: {words}")
+
+# 6. Функція find() для знаходження індексу першого входження певного слова.
+
+index_of_meat_hook = text.find("Meat Hook")
+print(f"Index of 'Meat Hook': {index_of_meat_hook}")
+
+# Ці функції надають різні можливості для роботи з текстом і відповідають вимогам щодо новизни двох функцій.
+# Передаю далі завдання Вікторії Чорномаз - використати три функції, дві з яких будуть відрізнятися від попередніх, наприклад startswith() та endswith().
